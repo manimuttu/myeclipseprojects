7b280daa58e5533efd3321a0f820ace1aa480215
manimuttu <manimuttu@gmail.com>
2016-02-03 14:25:41 +0800

    array with skipped index

---
+++ ineritance/src/ineritance/ArraylistRemoveExample.java
@@ -0,0 +1,20 @@
+package ineritance;
+
+
+public class ArraylistRemoveExample {
+
+	public static void main(String[] args) {
+		String[] list = new String[5];
+		
+		list[0] = "asdas";
+		list[1] = "asdas";
+		list[3] = "asdas";
+		list[4] = "asdas";
+		
+		for (String string : list) {
+			System.out.println(string);
+		}
+		
+		
+	}
+}
