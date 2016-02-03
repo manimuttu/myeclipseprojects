3a81c3b1bc91840376893a1f337009942455fbaf
manimuttu <manimuttu@gmail.com>
2016-02-03 14:26:36 +0800

    two interfaces

---
+++ ineritance/src/ineritance/TwoInterfaces.java
@@ -0,0 +1,20 @@
+package ineritance;
+
+interface interfaceone{
+	void samemethod();
+}
+
+interface interfacetwo {
+	void samemethod();
+}
+
+public class TwoInterfaces implements interfaceone, interfacetwo{
+
+	@Override
+	public void samemethod() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
